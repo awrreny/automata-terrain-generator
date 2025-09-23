9d172c558d5154d40e37b29ce6f39214b32d3fc2
awrreny <awrreny@gmail.com>
2025-09-23 12:01:10 +0100

    fixed type annotations for TerrainGrid

--- src/utils/grid.py
+++ src/utils/grid.py
@@ -1,24 +1,15 @@
-from data.terrain import terrains
-from typing import Tuple, Generator
+from data.terrain import TerrainType
+from typing import Iterator
 from utils.mapGen import generateGrid
-import random
 
 
 # grid containing TerrainType object from terrains dict
 class TerrainGrid:
-    def __init__(self, width, height, defaultTerrainType="forest") -> None:
+    def __init__(self, width, height) -> None:
         self.width = width
         self.height = height
 
-        # self.grid = [
-        #     [
-        #         terrains[random.choice(["grass", "grass", "grass", "forest", "sea"])]
-        #         for j in range(width)
-        #     ]
-        #     for i in range(height)
-        # ]
-        self.grid = generateGrid(width, height)
-
+        self.grid: list[list[TerrainType]] = generateGrid(width, height)
 
     
     def inBounds(self, x, y):
@@ -26,14 +17,14 @@ class TerrainGrid:
 
 
     # starts at 0, 0 at top left
-    def getTileByCoords(self, x, y):
+    def getTile(self, x, y):
         if not self.inBounds(x, y):
             raise ValueError(f"Coordinates ({x}, {y}) are out of bounds.")
         
         return self.grid[y][x]
     
 
-    def tiles(self) -> Generator[Tuple[int, int, str], None, None]:
+    def tiles(self) -> Iterator[tuple[TerrainType, int, int]]:
         for y, row in enumerate(self.grid):
             for x, tile in enumerate(row):
                 yield (tile, x, y)
