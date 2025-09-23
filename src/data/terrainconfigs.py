ed24be91383d98f900371eb91d6d6e8a4cc996b8
awrreny <awrreny@gmail.com>
2025-09-23 15:31:44 +0100

    made generateGrid choose terrain type randomly

--- src/data/terrainconfigs.py
+++ src/data/terrainconfigs.py
@@ -1,37 +1,38 @@
 from utils.neighbours import NeighbourStrategy
 
-DEFAULT_TERRAIN_CONFIG = {
-    "background": "grass",
-    "overlays": [
-        {
-            "terrain_type": "forest",
-            "initial_density": 0.45,
-            "survival_threshold": 0.3,
-            "birth_threshold": 0.65,
-            "iterations": 25,
-            "neighbour_strategy": NeighbourStrategy.WEIGHTED_DISTANCE
-        },
-        {
-            "terrain_type": "sea",
-            "initial_density": 0.09,
-            "survival_threshold": 0.1,
-            "birth_threshold": 0.35,
-            "iterations": 25,
-            "neighbour_strategy": NeighbourStrategy.WEIGHTED_DISTANCE
-        }
-    ]
-}
-
-DESERT_TERRAIN_CONFIG = {
-    "background": "sand",
-    "overlays": [
-        {
-            "terrain_type": "mountain",
-            "initial_density": 0.2,
-            "survival_threshold": 0.2,
-            "birth_threshold": 0.5,
-            "iterations": 10,
-            "neighbour_strategy": NeighbourStrategy.WEIGHTED_DISTANCE
-        }
-    ]
-}
+TERRAIN_CONFIGS = [
+    {
+        "background": "grass",
+        "overlays": [
+            {
+                "terrain_type": "forest",
+                "initial_density": 0.45,
+                "survival_threshold": 0.3,
+                "birth_threshold": 0.65,
+                "iterations": 25,
+                "neighbour_strategy": NeighbourStrategy.WEIGHTED_DISTANCE
+            },
+            {
+                "terrain_type": "sea",
+                "initial_density": 0.09,
+                "survival_threshold": 0.1,
+                "birth_threshold": 0.35,
+                "iterations": 25,
+                "neighbour_strategy": NeighbourStrategy.WEIGHTED_DISTANCE
+            }
+        ]
+    },
+    {
+        "background": "sand",
+        "overlays": [
+            {
+                "terrain_type": "mountain",
+                "initial_density": 0.2,
+                "survival_threshold": 0.2,
+                "birth_threshold": 0.5,
+                "iterations": 10,
+                "neighbour_strategy": NeighbourStrategy.WEIGHTED_DISTANCE
+            }
+        ]
+    }
+]
