d22bcecfd4bd8de9ba400d0c540b7af4963a3b73
awrreny <awrreny@gmail.com>
2026-01-04 18:28:00 +0000

    Refactor ColorMore: specific imports, tuple constants, and type hints

--- src/utils/colour.py
+++ src/utils/colour.py
@@ -1,21 +1,21 @@
 # each colour is a 3-tuple (R, G, B)
-import pygame as pg
+from pygame import Color
 
 
 
-class ColorMore(pg.Color):
-    _WHITE = pg.Color(255, 255, 255)
-    _BLACK = pg.Color(0, 0, 0)
+class ColorMore(Color):
+    WHITE = (255, 255, 255)
+    BLACK = (0, 0, 0)
 
-    def brighten(self, factor=0.1):
-        return ColorMore(self.lerp(ColorMore._WHITE, factor))
-
-
-    def darken(self, factor=0.1):
-        return ColorMore(self.lerp(ColorMore._BLACK, factor))
+    def brighten(self, factor: float = 0.1):
+        return ColorMore(self.lerp(ColorMore.WHITE, factor))
 
 
-    def desaturate(self, factor=0.1):
+    def darken(self, factor: float = 0.1):
+        return ColorMore(self.lerp(ColorMore.BLACK, factor))
+
+
+    def desaturate(self, factor: float = 0.1):
         return ColorMore(self.lerp(self.grayscale(), factor))
 
 
